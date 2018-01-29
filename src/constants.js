af88482fe49e563ae3b0bafabc40c7f6aafaf6b2
Jeremie Dayan <30652355+jeremie-stratumn@users.noreply.github.com>
2018-01-29 21:39:07 +0000

    add REACT_APP_ env variables (#16)

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,12 @@
 // API constants
 export const API_URL =
-  process.env.INDIGOTRACE_API_URL || 'https://staging-api.indigotrace.com';
-export const API_VERSION = process.env.INDIGOTRACE_API_VERSION || 'v1';
+  process.env.INDIGOTRACE_API_URL ||
+  process.env.REACT_APP_INDIGOTRACE_API_URL ||
+  'https://staging-api.indigotrace.com';
+export const API_VERSION =
+  process.env.INDIGOTRACE_API_VERSION ||
+  process.env.REACT_APP_INDIGOTRACE_API_VERSION ||
+  'v1';
 
 // ROUTE constants
 export const ROUTE_SDK_TRACES = `/workflow/${API_VERSION}/sdk/traces`;
